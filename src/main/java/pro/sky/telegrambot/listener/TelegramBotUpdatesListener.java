45564c39f8fb0782d816b63cf4e06db34bc83a07
intelcore <11mailliam11@mail.ru>
2024-09-03 18:58:28 +0300

    Commit

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -33,7 +33,58 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             LOGGER.info("Processing update: {}", update);
             // Process your updates here
 
+            if (update.callbackQuery() != null) {
+                if (update.callbackQuery().data().equals("stage1"))
+                    giveInfoAboutShelter(update);
+
+                if (update.callbackQuery().data().equals("stage2"))
+                    printInstructionsAboutPetFromShelter(update);
+
+                if (update.callbackQuery().data().equals("stage3"))
+                    getPetReport(update);
+
+                if(update.callbackQuery().data().equals("callVolunteer")){
+                    callVolunteer(update);
+                }
+            }
+
+            if (update.message() != null) {
+                // старт
+                if (update.message().text().equals("/start"))
+                    selectRequest(update);
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+
+    private void selectRequest(Update update) {
+        telegramBot.execute(new SendMessage(update.message().chat().id(),
+                "Приветствую! Я бот, помогаю взаимодействовать с приютами для собачек!"));
+
+        SendMessage sm = new SendMessage(update.message().chat().id(),
+                "Какой у вас вопрос?");
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        markup.addRow(new InlineKeyboardButton("Узнать информацию о приюте").callbackData("stage1"));
+        markup.addRow(new InlineKeyboardButton("Как взять животное из приюта").callbackData("stage2"));
+        markup.addRow(new InlineKeyboardButton("Прислать отчет о питомце").callbackData("stage3"));
+        markup.addRow(new InlineKeyboardButton("Позвать волонтёра").callbackData("callVolunteer"));
+
+        sm.replyMarkup(markup);
+        telegramBot.execute(sm);
+    }
+
+    private void giveInfoAboutShelter(Update update){ // здесь этап 1
+
+    }
+    private void printInstructionsAboutPetFromShelter(Update update){ // здесь этап 2
+
+    }
+    private void getPetReport(Update update){ // здесь этап 3
+
+    }
+    private void callVolunteer(Update update){ // позвать волонтёра
+
+    }
+
 }
---
+++ src/main/java/pro/sky/telegrambot/model/Volunteer.java
@@ -0,0 +1,6 @@
+package pro.sky.telegrambot.model;
+
+public class Volunteer {
+    private long id;
+    private String telegramNickname;
+}
