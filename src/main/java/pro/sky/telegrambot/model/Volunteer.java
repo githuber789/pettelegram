7fd65e203cbbd0b7fb450a25243de48f3ea86b74
intelcore <11mailliam11@mail.ru>
2024-09-03 19:01:23 +0300

    Commit

--- src/main/java/pro/sky/telegrambot/model/Volunteer.java
+++ src/main/java/pro/sky/telegrambot/model/Volunteer.java
@@ -1,6 +1,6 @@
 package pro.sky.telegrambot.model;
 
 public class Volunteer {
-    private long id;
-    private String telegramNickname;
+    private long id;//
+    private String telegramNickname;//
 }
